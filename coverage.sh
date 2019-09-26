42ffb44764f480eb20463dc69da3ac5c14565039
Amir Absolutely <mr.amirka@ya.ru>
2019-09-26 17:31:29 +0300

    added coverage

--- coverage.sh
+++ coverage.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-for file in target/debug/async_fs-*; do [ -x "${file}" ] || continue; mkdir -p "target/cov/$(basename $file)"; kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$file"; done && bash <(curl -s https://codecov.io/bash)
+for file in target/debug/async_fs-*; do [ -x "${file}" ] || continue; mkdir -p "target/cov/$(basename $file)"; kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$file"; done && bash <(curl -s https://codecov.io/bash)  -t $CODECOV_TOKEN
